fe5f729ddd4c4c84f521c90271d768cedaaf4b8d
Aonrafay <aonrafay@gmail.com>
2025-07-15 23:42:35 +0500

    Add files via upload

---
+++ Javascript Console API.js
@@ -0,0 +1,10 @@
+// 2-javascript console API
+console.log("Hello World");
+console.log(10 + 20);
+console.warn("This is a warning");
+console.error("This is an error");
+console.clear();
+console.assert(5 < 2, "This is an assertion error");
+console.table({name: "John", age: 30, city: "New York"});
+console.log("The course is avaiable.");
+alert("me");
