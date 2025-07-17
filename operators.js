fd4610d96e8e73b163be3565eed5e476b25a596e
Aonrafay <aonrafay@gmail.com>
2025-07-17 13:43:03 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,18 @@
+// operators in javascript
+// Arithmetic operators
+var a = 34;
+var b = 265;
+console.log("The sum of a and b is: " , a + b);
+console.log("The difference of a and b is: " , a - b);
+console.log("The product of a and b is: " , a * b);
+console.log("The quotient of a and b is: " ,a / b);
+console.log("The remainder of a and b is: " ,a % b);
+
+
+// comparison operators
+var x = 34;
+var y = 56;
+console.log(x == y);
+console.log(x != y);
+console.log(x > y);
+console.log(x < y);
