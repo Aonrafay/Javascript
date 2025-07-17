edf0dc70599d95950d8cb5d276b00d2d8f730a80
Aonrafay <aonrafay@gmail.com>
2025-07-17 13:43:29 +0500

    Add files via upload

---
+++ data tyes and objects.js
@@ -0,0 +1,28 @@
+// object
+
+var Marks= {
+    "John": 98, 
+    "Doe": 89,
+    "Jane": 95
+}
+console.log(Marks);
+
+var a=true; //boolean
+var b= false; 
+console.log(a, b);
+
+// two datatypes in JS
+// 1-primitive data types
+//string, number, boolean, null, undefined, symbol
+//2-reference data types
+//object, array, function
+
+var arr = [1, 2, 3, 4, 5]; //array
+ console.log(arr);
+
+ // logicl operators
+var p = true;
+var q = false;
+console.log(p && q); // logical AND
+console.log(p || q); // logical OR
+console.log(!p); // logical NOT
