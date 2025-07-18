9370809ffacff18afdb17f99980507936cfb7868
Aonrafay <aonrafay@gmail.com>
2025-07-18 22:35:33 +0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+
+function avg(a, b) {
+    return (a + b) / 2;
+}
+c1=(avg(10, 20));
+c2=(avg(30, 40));
+console.log("The average of 10 and 20 is: " + c1, " and the average of 30 and 40 is: " + c2);
+
