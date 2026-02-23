e1d4da001a313e44c22ca1ab73ae77b78c4c1d96
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-23 19:36:00 +0000

    add files

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId=134556
+let accountemail="aonrafay@gmail.com"
+var accountpassword="12345"
+// accountId=98762
+
+accountemail="asdfg@gmail.com"
+accountemail="kimuyre@gmail.com"
+accountemail="ertyy@gmail.com"
+accountemail="mjkll@gmail.com"
+accountpassword="45667"
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword])
