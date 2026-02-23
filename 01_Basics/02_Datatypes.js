5e19f49d9407ad8d9904234021686285676a17cd
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-23 20:06:35 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,8 @@
+"use strict"//treat all JS code as new version
+ 
+//alert="Hello" // this is not run in nodejs but can run in browers
+
+let name="Muhammad Aon Rafay"
+let age=12
+let isloggedin=false
+
