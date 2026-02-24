50cb3484e1cb0902dee1cd7b482a22c434733705
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-24 19:07:45 +0000

    ConversionOperations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,14 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+//score=null=>0
+//score=undefined=>NaN
+//"33asc"=>NaN
+//bool->true=>1
+//bool->false=>0
+
