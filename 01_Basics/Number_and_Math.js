439f5d416f9adaf57b476d3f16da24dfefbab7b6
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-25 20:30:43 +0000

    Math and Numbers

---
+++ 01_Basics/Number_and_Math.js
@@ -0,0 +1,24 @@
+// Number in javascript
+const number=23
+console.log(number);
+//Other way of declaration
+const ID=new Number(34)
+
+console.log(ID);
+
+console.log(ID.toFixed(1))
+console.log(ID.toString());
+console.log(ID.valueOf());
+
+//      Maths
+
+console.log(Math)
+console.log(Math.abs(-5));
+console.log(Math.round(4.98));
+console.log(Math.ceil(2.34));
+console.log(Math.floor(4.98));
+
+
+
+
+
