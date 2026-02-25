54aab642c6063f781e678c7552db388075d56519
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-25 19:34:37 +0000

    Stack an dheap in js

---
+++ 01_Basics/StackandHeapinJS.js
@@ -0,0 +1,21 @@
+//This is used for the stack(Primitive datatypes)
+let mygithub="Aon Rafay"
+let anothergithub=mygithub
+anothergithub="abc@gmail.com"
+
+console.log(mygithub)
+console.log(anothergithub)
+
+
+//This concept is used for the heap(Non_primitive datatypes)
+let user1={
+    email:"aonrafay@gmail.com",
+    userId:"F2024CS005"
+}
+
+let user2=user1
+
+user2.email="rafayaon@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
