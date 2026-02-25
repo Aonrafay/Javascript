2e5eca33a48a97e9e380e746957f3ad2b484f964
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-02-25 20:03:49 +0000

    Srings

---
+++ 01_Basics/StringinJs.js
@@ -0,0 +1,12 @@
+const name="Muhammad Aon Rafay"
+const number= 43
+
+console.log(name + number);
+console.log(`My name is ${name} and the number is ${number}`);
+// another way to initialize strings
+let email=new String("aonrafay@gmail.com")
+
+
+
+
+
